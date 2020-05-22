1d203417d79f543a8e60c283d3f4f9067be83b2a
Lucas Souza <lucas.souza@rockcontent.com>
2020-05-22 17:08:21 -0300

    mongo url in .env; create .env.sample

--- config/mongo-client.ts
+++ config/mongo-client.ts
@@ -1,7 +1,10 @@
+import { config } from "https://deno.land/x/dotenv/mod.ts";
 import { MongoClient } from "https://deno.land/x/mongo@v0.7.0/mod.ts";
 
+const { MONGO_URL } = config();
+
 const client = new MongoClient();
-client.connectWithUri("mongodb+srv://user:1234@cluster0-9hrlu.mongodb.net/test?retryWrites=true&w=majority");
+client.connectWithUri(MONGO_URL);
 
 const db = client.database("deno_api");
 export default db;
